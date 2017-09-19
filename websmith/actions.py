72d0892169982d5784c921df4ecfcdcf03023e89
Og B. Maciel <omaciel@ogmaciel.com>
2017-09-19 08:52:05 -0400

    Adding support for more action Verbs.
    This is still very much in flux but I am adding new 'verbs' to help
    interacting with dropdown and radio buttons.

--- websmith/actions.py
+++ websmith/actions.py
@@ -2,14 +2,27 @@
 import logging
 import threading
 
-from websmith.utils import perform_action_chain_move, wait_until_element_exists
+from websmith.utils import (
+    choose,
+    fill_form,
+    find_element,
+    find_elements,
+    perform_action_chain_move,
+    select,
+    wait_until_element_exists,
+)
+
+from selenium.webdriver.common.by import By
+
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 
 class Action(object):
+    '''Base class for all web actions.'''
     data = threading.local()
+    elements = []
 
     def __init__(self):
         LOGGER.info('ACTION: {} on {}'.format(
@@ -22,8 +35,51 @@ class Action(object):
         raise NotImplementedError()
 
 
-class Click(Action):
+class Check(Action):
+    '''Mark a checkbox as checked.
 
+    Borrowed from https://github.com/cobrateam/splinter
+
+    Example:
+        >>> Check('some-check-box')
+    '''
+
+    def __init__(self, name):
+        self.name = name
+
+        super(Check, self).__init__()
+
+    def _run(self):
+        locator = (By.NAME, self.name)
+        element = wait_until_element_exists(self.browser, locator)
+        element.check()
+
+
+class Choose(Action):
+    '''Mark a radiobox as checked.
+
+    Borrowed from https://github.com/cobrateam/splinter
+
+    Example:
+        >>> Choose('some-radiobox-name', 'some-radiobutton-value')
+    '''
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+        super(Choose, self).__init__()
+
+    def _run(self):
+        choose(self.browser, self.name, self.value)
+
+
+class Click(Action):
+    '''Click a web element.
+
+    Example:
+        >>> Click((By.NAME, 'commit'))
+    '''
     def __init__(self, target):
         self.target = target
 
@@ -33,8 +89,48 @@ class Click(Action):
         wait_until_element_exists(self.browser, self.target).click()
 
 
-class Hover(Action):
+class Fill(Action):
+    '''Fill a web element (by its name) with a value.
 
+    Example:
+        >>> Fill({'some-text-box': 'some value'})
+        >>> Fill({'some-check-box': True})
+        >>> Fill({'some-select-field': 'some-choice'})
+
+    You can also fill out an entire form:
+        >>> Fill({
+                'name': 'John Steinbeck',
+                'writer': True,
+            })
+    '''
+    def __init__(self, field_values):
+        self.field_values = field_values
+
+        super(Fill, self).__init__()
+
+    def _run(self):
+        fill_form(self.browser, self.field_values)
+
+
+class Find(Action):
+    '''Find a web element.
+    '''
+
+    def __init__(self, locator):
+        self.locator = locator
+
+        super(Find, self).__init__()
+
+    def _run(self):
+        self.element = find_element(self.browser, self.locator)
+
+
+class Hover(Action):
+    '''Move and hover the mouse focus to a web element.
+
+    Example:
+        >>> Hover((By.ID, 'account_name'))
+    '''
     def __init__(self, target):
         self.target = target
 
@@ -45,7 +141,11 @@ class Hover(Action):
 
 
 class Go(Action):
+    '''Load URL in the web browser.
 
+    Example:
+        >>> Go('https://www.google.com')
+    '''
     def __init__(self, url):
         self.url = url
 
@@ -55,8 +155,32 @@ class Go(Action):
         self.browser.get(self.url)
 
 
-class SendKeys(Action):
+class Select(Action):
+    '''Select a choice from a list of web elements.
 
+    Borrowed from https://github.com/cobrateam/splinter
+
+    Example:
+        >>> Select('some-choice-list', 'some-choice-value')
+        >>> Select('some-choice-list', 'some-choice-text', True)
+    '''
+    def __init__(self, name, value, by_text=False):
+        self.name = name
+        self.value = value
+        self.by_text = by_text
+
+        super(Select, self).__init__()
+
+    def _run(self):
+        select(self.browser, self.name, self.value, self.by_text)
+
+
+class SendKeys(Action):
+    '''Send value to a web element that can receive text input.
+
+    Example:
+        >>> SendKeys((By.ID, 'lst-ib'), 'WebSmith')
+    '''
     def __init__(self, target, value):
         self.target = target
         self.value = value
@@ -64,22 +188,53 @@ class SendKeys(Action):
         super(SendKeys, self).__init__()
 
     def _run(self):
-        self.browser.find_element(*self.target).send_keys(self.value)
+        element = wait_until_element_exists(self.browser, self.target)
+        element.send_keys(self.value)
+
+
+class Uncheck(Action):
+    '''Mark a checkbox as unchecked.
+
+    Borrowed from https://github.com/cobrateam/splinter
+
+    Example:
+        >>> Uncheck('some-check-box')
+    '''
+
+    def __init__(self, name):
+        self.name = name
+
+        super(Uncheck, self).__init__()
+
+    def _run(self):
+        locator = (By.NAME, self.name)
+        element = wait_until_element_exists(self.browser, locator)
+        element.uncheck()
 
 
 class Wait(Action):
+    '''Wait for a web element to become available.
 
+    Raise a TimeoutException is element is not found.
+
+    Example:
+        >>> Wait((By.ID, 'lst-ib'))
+    '''
     def __init__(self, target):
         self.target = target
 
         super(Wait, self).__init__()
 
     def _run(self):
-        return wait_until_element_exists(self.browser, self.target)
+        self.element = wait_until_element_exists(self.browser, self.target)
 
 
 class WaitAndClick(Action):
+    '''Wait for a web element to become available and click it.
 
+    Example:
+        >>> WaitAndClick((By.NAME, 'commit'))
+    '''
     def __init__(self, target):
         self.target = target
 
--- websmith/utils.py
+++ websmith/utils.py
@@ -1,5 +1,8 @@
+import time
+
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -33,6 +36,74 @@ def ajax_complete(browser):
     return not (jquery_active or angular_active)
 
 
+def choose(browser, name, value):
+    '''Select a radio button by value.
+
+    Example:
+        >>> choose('author', 'steinbeck')
+    '''
+    locator = (
+        By.XPATH,
+        '//input[contains(@type, "radio") and contains(@name, "{}")]'.format(
+            name))
+    elements = find_elements(browser, locator)
+    for element in elements:
+        if element.get_attribute('value') == value:
+            element.click()
+
+
+def fill_form(browser, field_values):
+    '''Fill out a web form with values.
+
+    Borrowed from https://github.com/cobrateam/splinter
+
+    Example:
+
+        >>> fill_form({'q': 'websmoth'})
+        >>> fill_form({
+                'name': 'John Steinbeck',
+                'writer': True,
+            })
+    '''
+    for name, value in field_values.items():
+        locator = (By.NAME, name)
+        element = wait_until_element_exists(browser, locator)
+        if (
+                element.get_attribute('type') in ['text', 'password'] or
+                element.get_attribute('tag_name') == 'textarea'):
+            element.send_keys(value)
+        elif element.get_attribute('type') == 'checkbox':
+            if value:
+                element.check()
+            else:
+                element.uncheck()
+        elif element.get_attribute('type') == 'radio':
+            choose(browser, name, value)
+        elif element.get_attribute('tag_name') == 'select':
+            element.select(value)
+        else:
+            element.value = value
+
+
+def find_element(browser, locator):
+    '''Locate a web element.
+
+    Example:
+        >>> find_element((By.ID, 'lst-ib'))
+    '''
+    return browser.find_element(*locator)
+    #  return wait_for_element(browser, locator)
+
+
+def find_elements(browser, locator):
+    '''Locate a web element.
+
+    Example:
+        >>> find_elements((By.TAG_NAME, 'select'))
+    '''
+    return browser.find_elements(*locator)
+
+
 def perform_action_chain_move(browser, locator):
     """Moving the mouse to the middle of an element specified by locator
     parameter
@@ -48,6 +119,23 @@ def perform_action_chain_move(browser, locator):
     scroll_into_view(browser, element)
     ActionChains(browser).move_to_element(element).perform()
     wait_for_ajax(browser)
+
+
+def select(browser, name, value, by_text=False):
+    '''Select a radio button by value.
+
+    Example:
+        >>> select('author', 'steinbeck')
+    '''
+    locator = (
+        By.XPATH,
+        '//select[contains(@name, "{}")]/option[{}]'.format(
+            name,
+            ('text()="{}"'.format(value)
+             if by_text
+             else 'contains(@value, "{}")'.format(value)),
+        ))
+    find_element(browser, locator).click()
 
 
 def scroll_page(browser):
@@ -77,6 +165,26 @@ def wait_for_ajax(browser, timeout=30, poll_frequency=0.5):
     ).until(
         ajax_complete, 'Timeout waiting for page to load'
     )
+
+
+def wait_for_element(browser, locator, timeout=12, poll_frequency=0.5):
+    '''
+    '''
+    try:
+        element = WebDriverWait(
+            browser, timeout, poll_frequency).until(
+                expected_conditions.presence_of_element_located(locator),
+                message='Element {} could not be found.'.format(locator[1]))
+        wait_for_ajax(browser, poll_frequency=poll_frequency)
+        return element
+    except TimeoutException as err:
+        print(
+            '{}: Waiting for element "{}" to display. {}'.format(
+                type(err).__name__,
+                locator[1],
+                err
+            ))
+        return None
 
 
 def wait_until_element(browser, locator, timeout=12, poll_frequency=0.5):
